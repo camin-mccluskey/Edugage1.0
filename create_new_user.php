8be9578d9423eaa791f7b933c1e2e4265c504730
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2016-07-12 22:43:17 +0100

    new db work
    cleaned up code. creates whole new db for each user. also logs personal
    details into login table. no need for user type tables now.

--- create_new_user.php
+++ create_new_user.php
@@ -11,55 +11,24 @@
 	$year		= $_POST["Year"];
 	$form		= $_POST["Form"];
 
-	$usertypetable = "$usertype";
+	//connect to login table
+	mysqli_select_db($connection,"login");
 
-	//create new user in `$usertype` table
-	$query = "INSERT INTO $usertypetable (username, first_name, surname, id , year, form)
-	VALUES ('$username', '$firstname', '$surname', NULL,'$year', '$form')";
-	$result = mysqli_query($connection,$query);
-
-	//check if user was successfully created
-	if (!isset($result)) {
-	//redirect to admin_homepage.html but with user not created text
-		$_SESSION['username'] = $username;
-		echo "user " . $username . " couldnt not be created";
-	}
-	//redirect to admin_homepage.html but with user created text
-	else {
-  //check the user type to determine the form the users table should time_take
-	if ($usertype == "students") {
-		//create table to store quizname and score pairs
-			$sql = "CREATE TABLE $username (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, quiz_name VARCHAR(45) NOT NULL,
-	  	score INT(100) NOT NULL, time_taken INT(100) NOT NULL)";
-			$query = mysqli_query($connection, $sql);
-		}
-	elseif ($usertype == "teachers") {
-		//create table to store ????
-			$sql = "CREATE TABLE $username (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, class_name VARCHAR(45) NOT NULL,
-	  	no_pupils INT(100) NOT NULL, subject VARCHAR(45) NOT NULL)";
-			$query = mysqli_query($connection, $sql);
-	}
-	else {
-		//create table to store  ????
-			$sql = "CREATE TABLE $username (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, quiz_name VARCHAR(45) NOT NULL,
-			score INT(100) NOT NULL, time_taken INT(100) NOT NULL)";
-			$query = mysqli_query($connection, $sql);
-	}
-			// create entry into login table
-if($query) {
-			$sql = "INSERT INTO login (id, username, password, usertype) VALUES (NULL, '$username', 'password', '$usertype')";
-			$query = mysqli_query($connection, $sql);
-			var_dump($query);
-if($query) {
-			$_SESSION['username'] = $username ;
-			echo "user " . $username . " was successfully created";
-		}
-	else {
-		echo $username . " could not be created";
-	}
-		}
-	else {
-		echo $username . " could not be created";
-		}
+	// Create user database
+		$sql = "CREATE DATABASE $username";
+		$query = mysqli_query($connection,$sql);
+		if ($query) {
+				// Create entry in login table
+				$sql = "INSERT INTO login (id, username, password, usertype, first_name, surname, year, form) VALUES (NULL, '$username', 'password', '$usertype', '$firstname', '$surname', '$year', '$form')";
+				$query = mysqli_query($connection, $sql);
+					if ($query) {
+						echo "User Created";
 					}
+						else {
+							echo "User could not be created";
+						}
+				}
+		else {
+		echo "user exists, please chose another username";
+	}
 ?>
